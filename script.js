708d401ffe82a6e3b13934e884dda3ed62b5a588
Artem <khaapt@gmail.com>
2017-06-03 21:27:31 +0300

    search engine

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
 document.addEventListener('DOMContentLoaded', function () {
+
+	//variables for creating list
+	let videoList;
+	const list = document.querySelector('.watch-list');
+	const button = document.querySelector('button');
+	
+	//variables for starting video
+	const video = document.querySelector('.video-stream');
+	const title = document.getElementById('watch-title');
+	const time = document.getElementById('watch-time');
+	const view = document.getElementById('watch-view');
+
+	//variables for adding video
+  const form = document.getElementById('upload-form');
+  let titleform = document.getElementsByName("title")[0];
+  let scoreform = document.getElementsByName("score")[0];
+  let urlform = document.getElementsByName("url")[0];
+  let submit = document.getElementsByName("submit")[0];
+	console.log(scoreform.value, titleform.value, urlform.value)
+
+	//variables for search
+  const searchform = document.getElementById('searchform');
+  const searchinput = document.getElementsByName('searchinput')[0];
+
 	
 	// 
 	// functions for localStorage 
@@ -53,31 +77,12 @@ document.addEventListener('DOMContentLoaded', function () {
   }
   
 	
-	//variables for creating list
-	let videoList;
-	const list = document.querySelector('.watch-list');
-	const button = document.querySelector('button');
-	
-	//variables for starting video
-	const video = document.querySelector('.video-stream');
-	const title = document.getElementById('watch-title');
-	const time = document.getElementById('watch-time');
-	const view = document.getElementById('watch-view');
-
-	//variables for adding video
-  const form = document.getElementById('upload-form');
-  let titleform = document.getElementsByName("title")[0];
-  let scoreform = document.getElementsByName("score")[0];
-  let urlform = document.getElementsByName("url")[0];
-  let submit = document.getElementsByName("submit")[0];
-	console.log(scoreform.value, titleform.value, urlform.value)
-
 	function initializeApp(){
 		videoList = getVideoList();
 		if (videoList.length === 0) {
 			pullDataServer();
 		} else {
-	  	onDataReady();
+	  	onDataReady(videoList);
 		}
 	}
 	
@@ -94,7 +99,7 @@ document.addEventListener('DOMContentLoaded', function () {
       if(request.readyState === 4 && request.status === 200) {
         console.log('parse json from server (pullDataServer');
         videoList = JSON.parse(request.responseText);
-	  		onDataReady();
+	  		onDataReady(videoList);
       } else {
         console.log(request.statusText, request.readyState);
       }
@@ -106,34 +111,31 @@ document.addEventListener('DOMContentLoaded', function () {
 	// calling creating video-list function
 	//
 
-	function onDataReady(){
+	function onDataReady(list){
 		
-		if (videoList[0]) {
+		if (list[0]) {
 
-			for (var i = videoList.length - 1; i >= 0; i--) {
-				// console.log(videoList[i].header);
-				createListItem(videoList[i]);
+			for (var i = list.length - 1; i >= 0; i--) {
+				createListItem(list[i]);
 			}
 
 		} else {
 
-			let playlist = videoList.data.children;
-			for (var i = playlist.length - 1; i >= 0; i--) {
-				let item = playlist[i].data;
-				if (item.domain === "youtube.com"){
-					let info = {
-						header: item.title,
-						score: item.score,
-						img: item.media.oembed.thumbnail_url,
-						url: item.url,
-						created: item.created_utc,
-					}
-				createListItem(info);
-				saveVideoList(info);
+				let playlist = list.data.children;
+				for (var i = playlist.length - 1; i >= 0; i--) {
+					let item = playlist[i].data;
+					if (item.domain === "youtube.com"){
+						let info = {
+							header: item.title,
+							score: item.score,
+							img: item.media.oembed.thumbnail_url,
+							url: item.url,
+							created: item.created_utc,
+						}
+					createListItem(info);
+					saveVideoList(info);
+				}
 			}
-		}
-
-
 		}
 
 		let firstVideo = document.querySelector('.video-item');
@@ -233,16 +235,46 @@ document.addEventListener('DOMContentLoaded', function () {
 	});
 
 	// 
+	// search engine
+	// 
+
+	function search (query) {
+		if (videoList) {
+			let newArr = [];
+			for (let i = 0; i < videoList.length; i++) {
+				let match = isQueryIncluded(videoList[i].header, query)
+				if (match) {
+					newArr.push(videoList[i]);
+				}
+			}
+			searchinput.value = '';
+			return newArr;
+		}
+	}
+
+	function isQueryIncluded(headerOf, query) {
+		let regex = new RegExp(query, 'i');
+		return headerOf.match(regex);
+	}
+	
+
+	searchform.addEventListener('submit', function(e) {
+		e.preventDefault();
+		let query = searchinput.value;
+		list.innerHTML = '';
+		onDataReady(search(query));
+	});
+	
+	// 
 	// pure functions for parsing videoId and Time
 	// 
 
 	function parseId(url){
-	    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
-	    let match = url.match(regExp);
-	    return (match&&match[7].length==11)? match[7] : false;
-		}
-		
-	// console.log('parseId', parseId('https://www.youtube.com/watch?v=b-v-lTtS_os'));
+    let regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+    let match = url.match(regExp);
+    return (match&&match[7].length==11)? match[7] : false;
+	}
+	
 
 	function getTime(ut) {
 		let months = ['января', 'февраля', 'марта', 'апреля', 'мая', 'июня', 'июля',
